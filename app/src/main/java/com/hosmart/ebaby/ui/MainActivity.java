94607ead78407fef5c7694677bc334ed0b355c9a
sayid <sayid88927@gmail.com>
2020-07-02 03:08:25 +0800

    添加音视频

--- app/src/main/java/com/hosmart/ebaby/ui/MainActivity.java
+++ app/src/main/java/com/hosmart/ebaby/ui/MainActivity.java
@@ -433,6 +433,7 @@ public class MainActivity extends BaseActivity implements MainContract.View, Che
         Button btnCancel = view.findViewById(R.id.btn_cancel);
         Button btnOk = view.findViewById(R.id.btn_ok);
         btnCancel.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 if (dialog.isShowing()) {
